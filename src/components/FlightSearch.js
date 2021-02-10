00a73fa1b8de115ecda956b30b671491e5d5cbd6
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2021-02-10 17:37:03 +1100

    turning search text inputs to dropdown menu

--- src/components/FlightSearch.js
+++ src/components/FlightSearch.js
@@ -1,42 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import _ from "underscore";
 
 export class FlightSearch extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super() 
-        this.state = {
-            search: [
-                {id: '1', }
-            ]
-        }
-    }
+    this.state = {
+      departure: "",
+      arrival: "",
+    };
 
-    _handleSubmit(e) {
-        e.preventDefault();
-    }
+    this._handleInput = this._handleInput.bind(this);
+  }
 
-    _handleInput(e) {
-      
-    }
+  _handleInput(e) {
+    const target = e.target;
+    const value = target.type === "text";
+    const name = target.name;
 
-    render() {
-        return (
-            <div className="bg-red-500 rounder-md text-white my-10 p-4">
-                <h2>Search Flights</h2>
-                <form onSubmit={this._handleSubmit}>
-                    <label htmlFor="departure">From 
-                    <input type="text" name="departure" placeholder="Departure City" /></label>
-                    
-                    <label htmlFor="arrival">To 
-                        <input type="text" name="arrival" placeholder="Arrival City" />
-                    </label>
-                    <button className="bg-indigo-500 text-white rounded-md px-4 py-2 m-2">Search Flight</button>
-                </form>
-            </div>
-        )
-    }
+    this.setState({ [name]: value });
+  }
+
+  _handleSubmit(e) {
+    console.log(e.target);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="bg-red-500 rounder-md text-white my-10 p-4">
+        <h2>Search Flights</h2>
+        <form onSubmit={this._handleSubmit}>
+          <label htmlFor="departure">
+            From:
+            <select name="departure" id="departure" required>
+              <option value="">--Please choose an option--</option>
+              <option value="brisbane">Brisbane</option>
+              <option value="sydney">Sydney</option>
+              <option value="perth">Perth</option>
+              <option value="darwin">Darwin</option>
+              <option value="melbourne">Melbourne</option>
+              <option value="hobart">Hobart</option>
+            </select>
+          </label>
+
+          <label htmlFor="arrival">
+            To:
+            <select name="arrival" id="arrival" required>
+              <option value="">--Please choose an option--</option>
+              <option value="brisbane">Brisbane</option>
+              <option value="sydney">Sydney</option>
+              <option value="perth">Perth</option>
+              <option value="darwin">Darwin</option>
+              <option value="melbourne">Melbourne</option>
+              <option value="hobart">Hobart</option>
+            </select>
+          </label>
+          <button
+            type="submit"
+            value="submit"
+            className="bg-indigo-500 text-white rounded-md px-4 py-2 m-2"
+          >
+            Search Flight
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default FlightSearch
-
-
+export default FlightSearch;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,33 +1,24 @@
-import React, { Component } from 'react'
-import FlightSearch from '../components/FlightSearch'
-import SearchResults from '../components/SearchResults'
+import React, { Component } from "react";
+import FlightSearch from "../components/FlightSearch";
+import SearchResults from "../components/SearchResults";
 
 export class Search extends Component {
-    constructor() {
-        super()
-        this.state = {
-            undefined: undefined
-        }
-    }
-    
+  constructor() {
+    super();
+    this.state = {
+      undefined: undefined,
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <h1 className="text-5xl">Search Flights</h1>
-                <FlightSearch />
-                <SearchResults />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1 className="text-5xl">Search Flights</h1>
+        <FlightSearch />
+        <SearchResults />
+      </div>
+    );
+  }
 }
 
-export default Search
-
-
-100
-for (let i = 0; i < this.props.max_seats; i++) {
-    const seat = this.props.max_seats[i];
-    return <Seat props={seat}></Seat>
-
-}
+export default Search;
