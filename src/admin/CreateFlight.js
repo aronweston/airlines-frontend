6ddd28b3312d2835e13fafa0de150410cae9a95c
aronweston <aron@firststepdigital.com.au>
2021-02-11 12:42:16 +1100

    working flights page that will search the data once, working on polling in next commit

--- src/admin/CreateFlight.js
+++ src/admin/CreateFlight.js
@@ -1,52 +1,65 @@
 import React, { Component } from 'react'
 
 export class CreateFlight extends Component {
-
+   
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            isAdmin: false
-        }
-        this.isAdmin = false;
-    }
+            departure: "",
+            arrival: ""
+        };
 
-    //TODO: bring in the planes data from the server and loop through them as values for the plane choice like in Rails
+        this._handleDeparture = this._handleDeparture.bind(this);
+        this._handleArrival = this._handleArrival.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
+
+  }
+  
+  _handleDeparture(e) {
+    this.setState({ departure: e.target.value });
+  }
+
+  _handleArrival(e) {
+    this.setState({ arrival: e.target.value });
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.departure, this.state.arrival)
+  }
+
     render() {
-        if (this.isAdmin) {
-            return (
-                <div className='bg-red-500 rounder-md text-white my-10 p-4'>
-                    Create Flight
-                    <form onSubmit={this._handleSubmit}>
-                        <label htmlFor="flight-number">Flight Number
-                        <input type="number" name="flightnumber" placeholder="Flight Number" /></label>
+        return (
+            <div className='bg-red-500 rounder-md text-white my-10 p-4'>
+                Create Flight
+                <form onSubmit={this._handleSubmit}>
+                    <label htmlFor="flight-number">Flight Number
+                    <input type="number" name="flightnumber" placeholder="Flight Number" /></label>
 
-                        <label htmlFor="departure">From
-                        <input type="text" name="departure" placeholder="Departure City" /></label>
-                        
-                        <label htmlFor="arrival">To
-                            <input type="text" name="arrival" placeholder="Arrival City" />
-                        </label>
+                    <label htmlFor="departure">From
+                    <input type="text" name="departure" placeholder="Departure City" /></label>
+                    
+                    <label htmlFor="arrival">To
+                        <input type="text" name="arrival" placeholder="Arrival City" />
+                    </label>
 
-                        <label htmlFor="">Date
-                            <input type="date" name="date" />
-                        </label>
+                    <label htmlFor="">Date
+                        <input type="date" name="date" />
+                    </label>
 
-                        <label htmlFor="plane">Plane:
-                            <select>
-                                <option value="" selected>-</option>
-                                <option value="747">Boeing 747</option>
-                                <option value="A380">Airbus A380</option>
-                                <option selected value="A320">Airbus A320</option>
-                            </select>
-                        </label>
+                    <label htmlFor="plane">Plane:
+                        <select>
+                            <option value="" selected>-</option>
+                            <option value="747">Boeing 747</option>
+                            <option value="A380">Airbus A380</option>
+                            <option selected value="A320">Airbus A320</option>
+                        </select>
+                    </label>
 
-                        <button className="bg-indigo-500 text-white rounded-md px-4 py-2 m-2">Create Flight</button>
-                    </form>
-                </div>
-            )
-        } else {
-            return null;
-        }   
+                    <button className="bg-indigo-500 text-white rounded-md px-4 py-2 m-2">Create Flight</button>
+                </form>
+            </div>
+        )
     }
 }
 
--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import _ from 'underscore'
-
+import {Link} from 'react-router-dom';
 
 const SERVER_URL = 'http://localhost:3000/flights.json'
 
 export class Flights extends Component {
-    getFlights(depart, arrival) {
-    axios.get(SERVER_URL)
-      .then(res => {
-        const results = _(res.data).where({departure: depart, arrival: arrival})
-        this.setState({ results: results })
-        console.log(this.state.results);
-      })
-      .catch(err => console.error(err))
-  }
+
+    constructor() {
+        super()
+        this.state = {
+            flights: []
+        }
+   
+
+        
+    }
+    
+    componentDidMount() {
+        const getFlights = () => {
+            axios.get(SERVER_URL)
+            .then(res => {
+                console.log(res.data);
+                this.setState({ flights: res.data })
+                console.log(this.state.flights);
+            })
+            .catch(err => console.error(err))
+        }
+        getFlights();
+    }
+
 
     render() {
         return (
             <div>
-                <h1 className="text-5xl">Flights</h1>
-    
-                <table className="table-auto">
+                <h1 className="text-5xl ">Flights</h1>
+                <table className="p-10 m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
                 <thead>
-                    <tr>
+                    <tr className="text-left p-10 border-b-2 border-gray-300">
                         <th>Date</th>
                         <th>Flight No.</th>
                         <th>Departure</th>
@@ -32,16 +45,18 @@ export class Flights extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>3/1/13</td>
-                        <td>23</td>
-                        <td>Melbourne</td>
-                        <td>Adelaide</td>
-                        <td>747</td>
+                {this.state.flights && this.state.flights.map((flight) => 
+                    <tr key={flight.id} className="">
+                        <td>{new Date(flight.date).toLocaleDateString()}</td>
+                        <Link to={`flights/${flight.id}`}><td>{flight.flight_number}</td></Link>
+                        <td>{flight.departure.toUpperCase()}</td> 
+                        <td>{flight.arrival.toUpperCase()}</td>
+                        <td>{flight.arrival.toUpperCase()}</td>
                     </tr>
+                 )}        
                 </tbody>
                 </table>
-                </div>
+            </div>
         )
     }
 }
