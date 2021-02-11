3f48f57fc18640c50b190df45a398d5a0b04bf93
aronweston <aron@firststepdigital.com.au>
2021-02-11 13:23:48 +1100

    table changes for the flights page

--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -29,28 +29,29 @@ export class Flights extends Component {
     render() {
         return (
             <div>
-                <h1 className="text-5xl ">Flights</h1>
-                <table className="p-10 m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
-                <thead>
-                    <tr className="text-left p-10 border-b-2 border-gray-300">
-                        <th>Date</th>
-                        <th>Flight No.</th>
-                        <th>Departure</th>
-                        <th>Arrival</th>
-                        <th>Plane</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {this.state.flights && this.state.flights.map((flight) => 
-                    <tr key={flight.id} className="text-left border-b-2 border-gray-300 rounded-b">
-                        <td className="px-4 py-3">{new Date(flight.date).toLocaleDateString()}</td>
-                        <td className="px-4 py-3"><Link className="hover:underline" to={`flights/${flight.id}`}>{flight.flight_number}</Link></td>
-                        <td className="px-4 py-3">{flight.departure.toUpperCase()}</td> 
-                        <td className="px-4 py-3">{flight.arrival.toUpperCase()}</td>
-                        <td className="px-4 py-3">{flight.plane.toUpperCase()}</td>
-                    </tr>
-                 )}        
-                </tbody>
+                <h1 className="text-5xl my-2">Flights</h1>
+                <table className="table-fixed bg-gray-200 text-gray-800">
+                    <thead>
+                        <tr className="text-left p-10 border-b-2 border-gray-300">
+                            <th className="w-1/5">Departure</th>
+                            <th className="w-1/5">Arrival</th>
+                            <th className="w-1/5"> Date</th>
+                            <th className="w-1/5">Plane</th>
+                            <th className="w-1/5">Flight No.</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                    {this.state.flights && this.state.flights.map((flight) => 
+                        <tr key={flight.id} className="text-left border-b-2 border-gray-300 rounded-b">
+                            <td className="px-4 py-3">{flight.departure.toUpperCase()}</td> 
+                            <td className="px-4 py-3">{flight.arrival.toUpperCase()}</td>
+                            <td className="px-4 py-3">{new Date(flight.date).toLocaleDateString()}</td>
+                            <td className="px-4 py-3">{flight.plane.toUpperCase()}</td>
+                            <td className="px-4 py-3"><Link className="hover:underline" to={`flights/${flight.id}`}>{flight.flight_number}</Link></td>
+                        </tr>
+                    )}        
+                    </tbody>
                 </table>
             </div>
         )
--- src/pages/SingleFlight.js
+++ src/pages/SingleFlight.js
@@ -37,7 +37,7 @@ export default function SingleFlight() {
     }, [])
 
     return (
-        <div>
+        <>
             <div className="my-10">
                 <h1 className="text-5xl">{flightData.departure} to {flightData.arrival}</h1>
                 <h2>{flightData.flight_number} - { new Date(flightData.date).toLocaleDateString()}</h2>
@@ -50,8 +50,7 @@ export default function SingleFlight() {
             <div className="my-10">
                 <Link className="border border-red-500 bg-red-500 text-white rounded-md px-4 py-2  transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outliner" to="/search">Search Flights</Link>
                 <Link className="border border-gray-200 bg-gray-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-600 focus:outline-none focus:shadow-outliner" to="/flights">Back to flights</Link>
-            </div>
-           
-        </div>
+            </div> 
+        </>
     )
 }
